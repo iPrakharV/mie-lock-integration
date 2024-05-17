8ae2aea7175a020988c5a5c484a109ea05f27ba3
Abhisar Anand <abhisar.muz@gmail.com>
2024-05-17 03:03:39 -0400

    Got RFID working

--- rfid_test.py
+++ rfid_test.py
@@ -3,12 +3,6 @@ from mfrc522 import SimpleMFRC522
 
 rfid = SimpleMFRC522()
 
-counter = 0
-
 while True:
-        
-        if counter % 1000 == 0:
-            print("Reading RFID...")
-
-        id, text = rfid.read()
-        print(f"ID: {id}\nText: {text}")
+    id, text = rfid.read()
+    print(f"ID: {id}\nText: {text}")
