8ffbe7ade53ec72ea2707fc3f51f755363fd502d
Abhisar Anand <abhisar.muz@gmail.com>
2024-05-17 02:28:16 -0400

    Added counter to rfid_test to ensure the program is running

--- rfid_test.py
+++ rfid_test.py
@@ -3,6 +3,12 @@ from mfrc522 import SimpleMFRC522
 
 rfid = SimpleMFRC522()
 
+counter = 0
+
 while True:
+        
+        if counter % 1000 == 0:
+            print("Reading RFID...")
+
         id, text = rfid.read()
         print(f"ID: {id}\nText: {text}")
